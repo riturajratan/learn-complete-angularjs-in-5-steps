e73d684db94c63b9e36ebc09ac7a3de4ec51c4f7
Rituraj <riturajratan@gmail.com>
2014-09-03 16:40:21 +0530

    set folder structure

--- step-5/app.js
+++ step-5/app.js
@@ -1,28 +1,54 @@
-//create a module
+//create a module myApp
 var myApp = angular.module('myApp', []);
-//url routing
+
+//Now Configure  our  routing
 myApp.config(function ($routeProvider, $locationProvider) {
     console.log("route");
     $routeProvider
+    // set route for the home page
     .when('/home',
     {
         controller: 'HomeCtrl',
         templateUrl: 'views/home.html'
     })
+
+     // set route for the about page
+
     .when('/about',{
         controller: 'AboutCtrl',
         templateUrl: 'views/aboutus.html'
-    }).
-      otherwise({
+    })
+
+   // set route for the contact page
+
+    .when('/contact',{
+        controller: 'ContactCtrl',
+        templateUrl: 'views/contact.html'
+    })
+
+    // if not match with any route config then send to home page
+
+     .otherwise({
         redirectTo: '/home'
       });
 
 
 });
 
-myApp.controller('RoutCtrl',function($scope, $location){
-  $scope.goto=function(view){
-      $location.path(view);
-  };
+// create the controller and inject Angular's $scope
 
-});
+  // set for Home Controller
+  myApp.controller('HomeCtrl', function($scope) {
+    // create a message to display in our view
+    $scope.message = "(',')---I am on Home page---(',')";
+  });
+
+// set for About Controller
+  myApp.controller('AboutCtrl', function($scope) {
+    $scope.message = "(',')---I am on About page---(',')";
+  });
+
+// set for Contact Controller
+  myApp.controller('ContactCtrl', function($scope) {
+    $scope.message = "(',')---I am on Contact page---(',')";
+  });
